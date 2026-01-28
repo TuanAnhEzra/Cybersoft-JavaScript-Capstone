194b14d319866887d6d257cfa9b0b87ba8993b98
TuanAnhEzra <nguyentuananh15102005@gmail.com>
2026-01-28 23:38:05 +0700

    Update homepage css draft

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,18 +4,22 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <h1>Welcome to Phone Shop</h1>
-      <p>Choose your role to continue</p>
-
-      <div className="button-group">
-        <Link to="/customer" className="btn btn-user">
-          User / Customer Page
-        </Link>
-
-        <Link to="/admin" className="btn btn-admin">
-          Admin Panel
-        </Link>
+    <div className="row mt-5">
+      <div className="col-md-6">
+        <div className="card text-center">
+          <div className="card-body">
+            <h3>For Customers</h3>
+            <Link to="/customer" className="btn btn-primary btn-lg">Shop Now</Link>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-6">
+        <div className="card text-center">
+          <div className="card-body">
+            <h3>For Admin</h3>
+            <Link to="/admin" className="btn btn-danger btn-lg">Manage Store</Link>
+          </div>
+        </div>
       </div>
     </div>
   );
