e952e1e58facb44ba0671eb93c3a91c6055a75b3
TuanAnhEzra <nguyentuananh15102005@gmail.com>
2026-01-28 23:39:19 +0700

    Homepage css draft

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,20 +4,25 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className="row mt-5">
-      <div className="col-md-6">
-        <div className="card text-center">
-          <div className="card-body">
-            <h3>For Customers</h3>
-            <Link to="/customer" className="btn btn-primary btn-lg">Shop Now</Link>
+    <div className="home-container">
+      <h1>Welcome to Phone Shop</h1>
+      <p>Choose your role to continue</p>
+
+      <div className="row mt-5">
+        <div className="col-md-6">
+          <div className="card text-center">
+            <div className="card-body">
+              <h3>For Customers</h3>
+              <Link to="/customer" className="btn btn-primary btn-lg">Shop Now</Link>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="col-md-6">
-        <div className="card text-center">
-          <div className="card-body">
-            <h3>For Admin</h3>
-            <Link to="/admin" className="btn btn-danger btn-lg">Manage Store</Link>
+        <div className="col-md-6">
+          <div className="card text-center">
+            <div className="card-body">
+              <h3>For Admin</h3>
+              <Link to="/admin" className="btn btn-danger btn-lg">Manage Store</Link>
+            </div>
           </div>
         </div>
       </div>
