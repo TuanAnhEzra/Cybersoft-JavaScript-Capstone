656f5d5644cdb40caf23534173d9871a298bc238
TuanAnhEzra <nguyentuananh15102005@gmail.com>
2026-01-28 23:28:38 +0700

    Add homepage, update customer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './home/Home';
 import Customer from './customer/Customer';
-import Admin from './admin/admin';
 import Cart from './customer/Cart';
+import Admin from './admin/Admin';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Customer/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-        <Route path="/admin" element={<Admin/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<div>404 - Page Not Found</div>} />
       </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
